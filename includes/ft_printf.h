be06bbfbfe07fe31fc5572754b624527882e62c7
Andrii STADNIK <astadnik@student.unit.ua>
2018-01-06 19:06:48 +0200

    Don't look here

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: astadnik <astadnik@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/03 14:05:42 by astadnik          #+#    #+#             */
-/*   Updated: 2018/01/06 11:00:57 by astadnik         ###   ########.fr       */
+/*   Updated: 2018/01/06 18:50:03 by astadnik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +17,27 @@
 
 typedef struct s_flag	t_flag;
 
-struct s_flag
+struct 					s_flag
 {
-	unsigned char	hash : 1;
-	unsigned char	zero : 1;
-	unsigned char	minus : 1;
-	unsigned char	plus : 1;
-	unsigned char	space : 1;
-	unsigned char	apostrophe : 1;
-	int				dollar;
-	char[2]			modif;
-	int				width;
-	int				prec;
-	char			conv;
-}
+	char	hash;
+	char	zero;
+	char	minus;
+	char	plus;
+	char	space;
+	char	apostrophe;
+	int		dollar;
+	char	modif[7];
+	char	system;
+	int		width;
+	int		prec;
+	char	conv;
+};
 
 int		ft_printf(const char *format, ...);
 int		ft_dprintf(int fd, const char *format, ...);
 int		ft_asprintf(char **ret, const char *format, ...);
 
-int		printf_make_str(char **ret, const char *format, va_list arg,
-		va_list arg_beg);
+int		printf_make_str(char **ret, const char *format, va_list arg);
 t_list	*printf_lstnew(void *content, size_t content_size);
 char	*printf_lsttostr(t_list	*head, size_t size);
 #endif
--- srcs/printf_flags.c
+++ srcs/printf_flags.c
@@ -6,8 +6,97 @@
 /*   By: astadnik <astadnik@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/05 19:01:49 by astadnik          #+#    #+#             */
-/*   Updated: 2018/01/05 19:02:15 by astadnik         ###   ########.fr       */
+/*   Updated: 2018/01/06 18:23:42 by astadnik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 
+const char	conv[] = "sSpdDioOuUxXcCeEfFgGaAnbrk"
+const char	flag[] = "#0-+ '"
+const char	mod[][] = {"z", "j", "ll", "l", "hh", "h", "L"}
+
+static int	printf_parse_flags(const char *start, int *i, t_flag *flags)
+{
+	if ft_strchr(flag, start[*i])
+	{
+		*((unsigned char *)flags + ft_strchr(flag, start[*(i++)]) - flag) = 1;
+		return (1);
+	}
+	return (0);
+}
+
+static void	printf_parse_num(const char *start, int *i, t_flag *flags)
+{
+	int	tmp;
+
+	tmp = ft_atoi(start + *i);
+	while (isdigit(start[*i]))
+		(*i)++;
+	if (start[*i] == '$')
+	{
+		flags.dollar = tmp;
+		(*i)++;
+	}
+	else if (start[*i] == '!')
+	{
+		flags.system = tmp > 16 ? 0 : tmp;
+		(*i)++;
+	}
+	else
+		flags.width = tmp;
+}
+
+static int	printf_parse_mod(const char *start, int *i, t_flag *flags)
+{
+	int	i;
+
+	i = 0;
+	while (i < 7)
+	{
+		if (!ft_strncmp(mod[i], start[*i], ft_strlen(mod[i])))
+		{
+			flags.modif[i] = 1;
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+/*
+** Parses flags
+*/
+
+t_flag			pritnf_parse_flags(const char *start, va_list *arg,
+		va_list arg_beg, int *i)
+{
+	int		tmp;
+	t_flag	flags;
+
+	ft_bzero(&flags, sizeof(t_flag));
+	while (!printf_parse_conv(start, i))
+	{
+		if (ft_strchr(flag, start[*i]))
+			*((char *)flags + ft_strchr(flag, start[*(i++)]) - flag) = 1;
+		else if (isdigit(start[*i]))
+			printf_parse_num(start, i, &flags);
+		else if (start[*i] == '*')
+		{
+			if (ft_isdigit(start[(*i)++]))
+			{
+				flags.width = printf_get_arg(ft_atoi(start + *i), arg_beg, );
+				while (ft_isdigit(start[*i]));
+			}
+			else
+				flags.width = va_args(*arg, int);
+		}
+		else if (!printf_parse_mod(start, i, &flags))
+		{
+			flags.conv = 'c';
+			break ;
+		}
+
+	}
+	return (flags);
+}
